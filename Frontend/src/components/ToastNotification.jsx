e8bc28d18096ddb5696645056b9212b256731ce6
Keval Dharmeshbhai Gandevia <kv476075@dal.ca>
2024-07-23 01:37:47 -0300

    Chore: add loading action

--- Frontend/src/components/ToastNotification.jsx
+++ Frontend/src/components/ToastNotification.jsx
@@ -1,20 +1,19 @@
 import { useToast } from "@chakra-ui/react";
 
 const ToastNotification = () => {
+  const toast = useToast();
 
-    const toast = useToast();
+  const showToast = ({ message, status }) => {
+    toast({
+      title: message,
+      status: status,
+      duration: 5000,
+      isClosable: true,
+      position: "top-right",
+    });
+  };
 
-    const showToast = ({ message, status }) => {
-      toast({
-        title: message,
-        status: status,
-        duration: 5000,
-        isClosable: true,
-        position: "top-right",
-      });
-    };
-  
-    return { showToast };
-}
+  return { showToast };
+};
 
-export default ToastNotification
+export default ToastNotification;
